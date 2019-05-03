86e432fabf743da968751ecc513f2857d2b8bc52
Vikas Ranjan <vranjan@mobileiron.com>
2019-05-03 20:59:42 +0530

    P238 pow in logn

---
+++ src/LeetCode/P50PowTest.java
@@ -0,0 +1,8 @@
+package LeetCode;
+
+public class P50PowTest {
+    public static void main(String[] args) {
+        P50Pow p50Pow = new P50Pow();
+        System.out.println(p50Pow.myPow(2,-2 ));
+    }
+}
