da74d31516653cdac75665f40fa452596d989371
Vikas Ranjan <vranjan@mobileiron.com>
2019-05-16 10:01:19 +0530

    Max sub array

---
+++ src/LeetCode/P53MaximumSubarray.java
@@ -0,0 +1,32 @@
+package LeetCode;
+
+/**
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+ *
+ * Example:
+ *
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * Follow up:
+ *
+ * If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+ */
+public class P53MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        if(nums.length == 0) {
+            return 0;
+        }
+        // Use kadane's algorithm (maximum sum is at that place or sum with previous place)
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = dp[0];
+
+        for(int i =1; i<nums.length; i++) {
+            dp[i] = nums[i] + (dp[i-1] > 0 ? dp[i-1] : 0);
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
+    }
+}
