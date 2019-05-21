a9f33f9095e6400968b690e2378f6b8d2d517b60
Vikas Ranjan <vranjan@mobileiron.com>
2019-05-21 14:22:19 +0530

    xor test

---
+++ src/General/XORTest.java
@@ -0,0 +1,12 @@
+package General;
+
+public class XORTest {
+
+    public static void main(String[] args) {
+
+        System.out.println(4^3);
+        System.out.println(4^5);
+        System.out.println(4^6);
+        System.out.println(4^2);
+    }
+}
