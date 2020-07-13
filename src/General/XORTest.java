f59aa1150ff11147bacc3442ed10b05eef1fb817
Vikas Ranjan <vranjan@groupon.com>
2020-07-13 10:43:40 +0530

    P136 single number

--- src/General/XORTest.java
+++ src/General/XORTest.java
@@ -3,10 +3,15 @@ package General;
 public class XORTest {
 
     public static void main(String[] args) {
+        int a = 0;
+        int[] arr = {4, 1, 4, 1, 2};
 
-        System.out.println(4^3);
-        System.out.println(4^5);
-        System.out.println(4^6);
-        System.out.println(4^2);
+       for(int i =0; i< arr.length; i++) {
+           System.out.println("a is "+a+ " arr is "+arr[i]);
+           a ^= arr[i];
+           System.out.println(" xor is "+a);
+       }
+
+        System.out.println(a);
     }
 }
---
+++ src/leetCodeTop100Liked/P136SingleNumber.java
@@ -0,0 +1,57 @@
+package leetCodeTop100Liked;
+
+/**
+ * Created by vranjan on 2020-07-13
+ *
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ *
+ * Note:
+ *
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ *
+ * Example 1:
+ *
+ * Input: [2,2,1]
+ * Output: 1
+ * Example 2:
+ *
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ */
+
+public class P136SingleNumber {
+    //    public int singleNumber(int[] nums) {
+    //        if (nums.length == 0) {
+    //            return 0;
+    //        }
+    //
+    //        // For even length integer
+    //        if (nums.length % 2 == 0) {
+    //            return 0;
+    //        }
+    //
+    //        Set<Integer> set = new HashSet<>();
+    //        for (final int num : nums) {
+    //            if (set.contains(num)) {
+    //                set.remove(num);
+    //            }
+    //            else {
+    //                set.add(num);
+    //            }
+    //        }
+    //
+    //        return set.iterator().hasNext() ? set.iterator().next() : 0;
+    //
+    //
+    //    }
+
+    // Approach 2 
+    public int singleNumber(int[] nums) {
+        int a = 0;
+        for (int x : nums) {
+            a ^= x;
+        }
+        return a;
+
+    }
+}
