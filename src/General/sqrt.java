b24767da3ff27303c41ff6b2be37f08f874396f8
Vikas Ranjan <vranjan@groupon.com>
2020-07-13 10:10:30 +0530

    Practice

---
+++ src/General/sqrt.java
@@ -0,0 +1,12 @@
+package General;
+
+/**
+ * Created by vranjan on 2020-06-19
+ */
+public class sqrt {
+
+    public static void main(String[] args) {
+        int x = -1000;
+        System.out.println(x*x);
+    }
+}
