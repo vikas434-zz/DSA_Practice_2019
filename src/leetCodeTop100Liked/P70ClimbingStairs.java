b618635d6fecb8d0319b28c5513fc9e317fcba40
Vikas Ranjan <vikasranjan008@gmail.com>
2020-07-25 11:26:01 +0530

    P70 climbing stairs

---
+++ src/leetCodeTop100Liked/P70ClimbingStairs.java
@@ -0,0 +1,30 @@
+package leetCodeTop100Liked;
+
+/* Created by Vikas Ranjan on 2020-07-25
+ *
+ */
+public class P70ClimbingStairs {
+
+    public int climbStairs(int n) {
+        if(n == 0) {
+            return 0;
+        }
+        if(n==1) {
+            return 1;
+        }
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for(int i = 2; i < n; i++) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        return dp[n-1];
+
+    }
+}
+
+/**
+ * At any stairs(except base case), it is combination of last two stairs.
+ */
